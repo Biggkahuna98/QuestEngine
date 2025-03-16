086634953e2b5e4a42a31548c5769d43738b80d2
Alex Sederquest <132014198+Biggkahuna98@users.noreply.github.com>
2025-03-16 10:34:56 -0400

    Fixed Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,10 +1,19 @@
 rd /s /q "ThirdParty"
 mkdir ThirdParty
 cd ThirdParty
-mkdir Engine
-cd Engine
 dir
 
 git clone git@github.com:Biggkahuna98/QuestEngineThirdPartyLibs.git
+
+dir
+
+mkdir Engine
+
+dir
+
+Robocopy "QuestEngineThirdPartyLibs" "Engine" /E /MOVE /NFL /NDL /NJH /NJS /nc /ns /np
+
+cd Engine
+rd /s /q ".git"
 
 dir
