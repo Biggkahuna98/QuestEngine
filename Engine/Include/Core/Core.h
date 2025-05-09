66c6aff833251eb43b9342311f938f85d5108672
Alex Sederquest <132014198+Biggkahuna98@users.noreply.github.com>
2025-05-09 16:52:29 -0400

    Added a dll import/export macro to Core.h

--- Engine/Include/Core/Core.h
+++ Engine/Include/Core/Core.h
@@ -3,5 +3,11 @@
 #include <cstdint>
 #include <memory>
 
+#ifdef QE_INTERNAL_USE_ONLY
+    #define QUEST_API __declspec(dllexport)
+#else
+    #define QUEST_API __declspec(dllimport)
+#endif
+
 #define QE_STRINGIFY(x) #x
 #define BIT(x) (1 << (x))
